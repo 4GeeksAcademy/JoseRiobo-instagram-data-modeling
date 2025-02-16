2c8baea87a6d5963f35942f33c07269db3f15b49
joseriobo <joseriobo7@gmail.com>
2025-02-16 19:16:45 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    id = Column(Integer, primary_key = True, nullable= False)
+    id = Column(Integer, primary_key = True)
     username = Column(String(250), nullable = False)
     firstname = Column(String(250), nullable = False)
     lastname = Column(String(250), nullable = False)
@@ -19,19 +19,20 @@ class Person(Base):
 
 class Follower(Base):
     __tablename__ = "follower"
-    user_from_id = Column(Integer, primary_key = True )
-    user_to_id = Column(Integer, ForeignKey("person.id"), nullable= False)
+    id = Column(Integer, primary_key = True )
+    user_from_id = Column(Integer, ForeignKey("person.id"))
+    user_to_id = Column(Integer, ForeignKey("person.id"))
     user = relationship(Person)
    
 class Post(Base):
     __tablename__ = "post"
-    id = Column(Integer,  primary_key=True, nullable= False)
-    user_id = Column(Integer, ForeignKey("person.id"), nullable= False)
+    id = Column(Integer,  primary_key=True)
+    user_id = Column(Integer, ForeignKey("person.id"))
     person = relationship(Person)
 
 class Media(Base):
     __tablename__ = "media"
-    id = Column(Integer, primary_key=True, nullable= False)
+    id = Column(Integer, primary_key=True)
     type = Column(Enum("type"), nullable= False)
     url = Column(String(250), nullable = False)
     post_id = Column(Integer,ForeignKey("post.id"))
@@ -39,7 +40,7 @@ class Media(Base):
 
 class Comment(Base):
     __tablename__ = "comment"
-    id = Column(Integer, primary_key=True, nullable= False)
+    id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable= False)
     author_id = Column(Integer, ForeignKey("person.id"))
     post_id= Column(Integer, ForeignKey("post.id"))
